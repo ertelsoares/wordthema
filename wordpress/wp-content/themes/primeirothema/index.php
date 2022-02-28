56a0386e7a0948a96ccdf5cbf195abf1e17e722f
Lucas Ertel Soares <lucassoares@alunos.utfpr.edu.br>
2022-02-28 19:11:59 -0300

    add conteudo de estrutura do tema dentro do index.php

---
+++ wordpress/wp-content/themes/primeirothema/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="profile" href="https://gmpg.org/xfn/11" />
+</head>
+
+<body <?php body_class(); ?>>
+    <div id="page" class="site">
+        <header>
+            <section class="top-bar1">
+                <div class="search">Pesquisa</div>
+                <div class="account">
+                    <div class="account-menu">Menu Conta</div>
+                    <div class="cart">Carrinho</div>
+                </div>
+            </section>
+            <section class="top-bar2">
+                <div class="brand">Logo</div>
+                <div class="site-menu">Menu</div>
+            </section>
+        </header>
+        <div class="content-area">
+            <main>
+                <section class="slider">Slider </section>
+                <section class="popular-products">
+                    <div class="section-title">
+                        <h2>Popular Products</h2>
+                    </div>
+                </section>
+                <section class="new-arrivals">
+                    <div class="section-title">
+                        <h2>New Arrivals</h2>
+                    </div>
+                </section>
+                <section class="deal-of-the-week">
+                    <div class="section-title">
+                        <h2>Deal of the Week </h2>
+                    </div>
+                </section>
+                <section class="site-blog">
+                    <div class="section-title">
+                        <h2>Latest News</h2>
+                    </div>
+                </section>
+            </main>
+        </div>
+        <footer>
+            <section class="footer-widgets"> Footer Widgets </section>
+            <section class="copyright">
+                <div class="copyright-text">
+                    <p>Copyright</p>
+                </div>
+                <nav class="footer-menu"> Menu rodapé</nav>
+            </section>
+        </footer>
+    </div>
+</body>
+
+</html>
